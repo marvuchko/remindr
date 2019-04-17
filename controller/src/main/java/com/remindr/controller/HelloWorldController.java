dbcb3587f4cec6c90eca7f3b6bd1c49c5c24d85c
Marko Vuckovic <marvuchko@gmail.com>
2019-04-17 02:42:45 +0200

    Initial commit

---
+++ controller/src/main/java/com/remindr/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.remindr.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldController extends ActionSupport {
+
+	private static final long serialVersionUID = -769309544238873160L;
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+
+}
